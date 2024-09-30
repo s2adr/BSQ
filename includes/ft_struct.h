22da240dc9033f16a9e871299e81074e78ea7fa6
saidriss <113543660+s2adr@users.noreply.github.com>
2024-09-30 12:21:28 +0200

    upload headers

---
+++ includes/ft_struct.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_struct.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saidriss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 11:19:58 by saidriss          #+#    #+#             */
+/*   Updated: 2024/09/30 11:20:01 by saidriss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STRUCT_H
+# define FT_STRUCT_H
+
+typedef struct s_map
+{
+	char	empty;
+	char	obs;
+	char	full;
+	char	**matrix;
+	int		is_valid;
+}	t_map;
+
+typedef struct s_square
+{
+	int	x_start;
+	int	y_start;
+	int	x_end;
+	int	y_end;
+}	t_square;
+
+#endif
