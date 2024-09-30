8f354dd119047d67dea4f737c68cfac30e5b0491
Saad Idrissi <saidriss@z3r12p1.42lyon.fr>
2024-09-30 13:23:47 +0200

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: saidriss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 11:59:42 by saidriss          #+#    #+#             */
-/*   Updated: 2024/09/30 12:39:46 by saidriss         ###   ########.fr       */
+/*   Updated: 2024/09/30 13:23:40 by saidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 void	ft_error(void)
 {
 	write (2, "Error\n", 6);
+	exit(1);
 }
 
 void	ft_error_map(void)
 {
 	write (2, "map error\n", 10);
-}
-
-void	ft_exit(void)
-{
-	ft_error_map();
-	exit(1);
+	exit(1)
 }
 
 int	main(int ac, char **av)
