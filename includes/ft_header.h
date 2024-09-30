77b4cbfa8e4a6f8ac2018775f78debb72a8ffce5
Saad Idrissi <saidriss@z3r12p1.42lyon.fr>
2024-09-30 16:03:06 +0200

    update header

--- includes/ft_header.h
+++ includes/ft_header.h
@@ -6,7 +6,7 @@
 /*   By: saidriss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 11:19:45 by saidriss          #+#    #+#             */
-/*   Updated: 2024/09/30 11:19:50 by saidriss         ###   ########.fr       */
+/*   Updated: 2024/09/30 15:56:02 by saidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,20 @@ void		ft_exit(void);
 int			ft_init(int argc, char *argv);
 char		**ft_split(char *str);
 
+// FT_MANAGE_FILE
+char		**ft_read_file(char *dictpath);
+char		**ft_read_stdin(void);
+int			ft_get_file_size(char *dictpath, int *file);
+
+// FT_CHECK_FILE
+int			ft_check_map_line_value(char **array_map, t_map map);
+int			ft_check_map_line_len(char **array_map);
+
 // FT_GET_MAP
 t_map		ft_get_map(int argc, char *argv);
 
 // FT_CHECK_MAP
 int			ft_is_map_valid(char **array_map, t_map map);
-
-// FT_CHECK_FILE
-int			ft_check_map_line_value(char **array_map, t_map map);
-int			ft_check_map_line_len(char **array_map);
 
 // FT_SOLVER
 t_map		ft_solver(t_map map);
@@ -43,19 +48,6 @@ t_square	find_square(t_map map, t_square square, int x_start, int y_start);
 int			square_size(t_square square);
 int			check_square(t_map map, t_square square);
 t_map		fill_square(t_map map, t_square square);
-
-// FT_MATRIX_SIZE
-int			x_size(char **matrix);
-int			y_size(char **matrix);
-
-// FT_FREE
-void		ft_free(char **dict);
-void		ft_free_map(t_map map);
-
-// FT_MANAGE_FILE
-char		**ft_read_file(char *dictpath);
-char		**ft_read_stdin(void);
-int			ft_get_file_size(char *dictpath, int *file);
 
 // TOOLS
 int			ft_strlen(char *str);
@@ -70,5 +62,13 @@ void		ft_putchar(char c);
 int			ft_is_printable(char c);
 int			ft_is_number(char c);
 int			ft_is_whitespace(char c);
+
+// FT_SIZE
+int			x_size(char **matrix);
+int			y_size(char **matrix);
+
+// FT_FREE
+void		ft_free(char **dict);
+void		ft_free_map(t_map map);
 
 #endif
